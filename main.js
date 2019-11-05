627854adf6e616e763ebff23fa57faf1ee69cbe7
Ellie Mawer <egmawer@gmail.com>
2019-11-05 20:03:06 +0000

    Add styles to input, button, title & tagline

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function myWeather() {
     $("#getWeather").html("");
     var cityName = $("#cityName").val();
 
-    var apiCall = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=f21c6b7c8c4e53536dd3c21d3c610573";
+    var apiCall = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=f21c6b7c8c4e53536dd3c21d3c610573&units=metric";
 
     $.getJSON(apiCall, weatherCallback);
 
@@ -12,6 +12,6 @@ function myWeather() {
         var temp = weatherData.main.temp;
         var description = weatherData.weather[0].description;
         console.log(weatherData.name, weatherData.sys.country, weatherData.main.temp, weatherData.weather[0].description);
-        $('#getWeather').append(cityName + " " + country + " " + temp + " " + description);
+        $('#getWeather').append(cityName + " " + country + " " + temp + "ºC" + " " + description);
     }
 };
