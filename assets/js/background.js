956005501a89d5acb6f609452256628836b329b3
Ellie Mawer <egmawer@gmail.com>
2019-12-05 21:08:01 +0000

    Add comments to code

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,9 +1,13 @@
+//Function for slideshow 
 var slides = document.querySelectorAll('#slides .slide');
 var currentSlide = 0;
 var slideInterval = setInterval(nextSlide, 5000);
 
 function nextSlide() {
+//change the current slide class to not appear
  slides[currentSlide].className = 'slide';
+ //cycle through slides, then back to 0 to create neverending loop
  currentSlide = (currentSlide + 1) % slides.length;
+ //change class to showing again
  slides[currentSlide].className = 'slide showing';
 }
